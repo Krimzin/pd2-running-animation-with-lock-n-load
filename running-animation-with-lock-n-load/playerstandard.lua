9750396e95d50187675418e8c07d5aa4ffb7b21b
Krimzin <9521585+Krimzin@users.noreply.github.com>
2024-04-09 20:12:14 +1200

    Change how the running animation is blocked

--- running-animation-with-lock-n-load/playerstandard.lua
+++ running-animation-with-lock-n-load/playerstandard.lua
@@ -1,7 +1,9 @@
 -- New members in PlayerStandard:
 -- _stop_running_anim_expire_t: number | nil
--- _ready_to_shoot_expire_t: number | nil
+-- _block_running_anim: boolean | nil
+-- _block_running_anim_expire_t: number | nil
 -- _anim_state: userdata (Idstring representing an animation state) | nil
+-- _anim_state_callback: function | nil
 
 local start_running_state = Idstring("fps/start_running")
 local running_state = Idstring("fps/running")
@@ -12,9 +14,7 @@ local function try_play_start_running(self)
 
 	if self._running
 		and not self._end_running_expire_t
-		and not self._shooting
-		and not self._ready_to_shoot_expire_t
-		and not self._anim_state
+		and not self._block_running_anim
 	then
 		local state = self._ext_camera:anim_state_machine():segment_state(self:get_animation("base"))
 
@@ -29,19 +29,15 @@ local function try_play_start_running(self)
 end
 
 local function try_play_stop_running(self, t)
-	local is_playing = false
 	local state = self._ext_camera:anim_state_machine():segment_state(self:get_animation("base"))
 
 	if (state == start_running_state) or (state == running_state) then
 		local speed_multiplier = self._equipped_unit:base():exit_run_speed_multiplier()
 		self._ext_camera:play_redirect(self:get_animation("stop_running"), speed_multiplier)
 		self._stop_running_anim_expire_t = t + (0.4 / speed_multiplier)
-		is_playing = true
-	elseif self._end_running_expire_t and (state == stop_running_state) then
-		self._stop_running_anim_expire_t = self._end_running_expire_t
-		is_playing = true
 	end
 
+	local is_playing = self._stop_running_anim_expire_t and true or false
 	return is_playing
 end
 
@@ -49,15 +45,14 @@ local _check_action_primary_attack = PlayerStandard._check_action_primary_attack
 function PlayerStandard:_check_action_primary_attack(t, input, params)
 	if self._equipped_unit:base():run_and_shoot_allowed() then
 		if input.btn_primary_attack_press then
-			-- Exit the ready-to-shoot state.
-			-- The player may have released the attack button before pressing it again.
-			self._ready_to_shoot_expire_t = nil
+			self._block_running_anim = true
+			self._block_running_anim_expire_t = nil
 			try_play_stop_running(self, t)
 		end
 
 		if input.btn_primary_attack_release then
-			-- Enter the ready-to-shoot state, it will last for 3 seconds.
-			self._ready_to_shoot_expire_t = t + 3
+			-- Unblock the running anim after 3 seconds.
+			self._block_running_anim_expire_t = t + 3
 		end
 
 		if self._stop_running_anim_expire_t and self._stop_running_anim_expire_t > t then
@@ -70,8 +65,12 @@ end
 
 local _check_action_deploy_underbarrel = PlayerStandard._check_action_deploy_underbarrel
 function PlayerStandard:_check_action_deploy_underbarrel(t, input)
-	if self._equipped_unit:base():run_and_shoot_allowed() then
-		if input.btn_deploy_bipod and self._running then
+	local run_and_shoot_allowed = self._equipped_unit:base():run_and_shoot_allowed()
+
+	if run_and_shoot_allowed then
+		if input.btn_deploy_bipod and self._equipped_unit:base():underbarrel_name_id() then
+			self._block_running_anim = true
+			self._block_running_anim_expire_t = nil
 			local is_playing = try_play_stop_running(self, t)
 
 			if is_playing then
@@ -84,20 +83,35 @@ function PlayerStandard:_check_action_deploy_underbarrel(t, input)
 		end
 	end
 
+	local toggle_underbarrel_wanted = self._toggle_underbarrel_wanted
 	local new_action = _check_action_deploy_underbarrel(self, t, input)
 
-	if new_action and self._equipped_unit:base():run_and_shoot_allowed() then
-		-- Assume that the underbarrel toggle animation is playing.
-		self._anim_state = self._ext_camera:anim_state_machine():segment_state(self:get_animation("base"))
+	if run_and_shoot_allowed then
+		if new_action then
+			-- Assume that the toggle underbarrel animation is playing,
+			self._anim_state = self._ext_camera:anim_state_machine():segment_state(self:get_animation("base"))
+			self._anim_state_callback = function (self, t)
+				self._block_running_anim_expire_t = t + 3
+			end
+		elseif input.btn_deploy_bipod or toggle_underbarrel_wanted then
+			-- Failed to toggle the underbarrel. Unblock the running animation.
+			self._block_running_anim = nil
+			try_play_start_running(self)
+		end
 	end
 
 	return new_action
 end
 
 Hooks:PostHook(PlayerStandard, "_start_action_reload", "RunningAnimationWithLockNLoad", function (self, t)
-	if self:_is_reloading() and self._equipped_unit:base():run_and_shoot_allowed() then
+	if self._equipped_unit:base():run_and_shoot_allowed() and self:_is_reloading() then
+		self._block_running_anim = true
+		self._block_running_anim_expire_t = nil
 		-- Assume that the reload animation is playing.
 		self._anim_state = self._ext_camera:anim_state_machine():segment_state(self:get_animation("base"))
+		self._anim_state_callback = function (self, t)
+			self._block_running_anim_expire_t = t + 3
+		end
 	end
 end)
 
@@ -118,13 +132,19 @@ Hooks:PreHook(PlayerStandard, "update", "RunningAnimationWithLockNLoad", functio
 		self._stop_running_anim_expire_t = nil
 	end
 
-	if self._ready_to_shoot_expire_t and self._ready_to_shoot_expire_t <= t then
-		self._ready_to_shoot_expire_t = nil
+	if self._block_running_anim_expire_t and self._block_running_anim_expire_t <= t then
+		self._block_running_anim_expire_t = nil
+		self._block_running_anim = nil
 		try_play_start_running(self)
 	end
 
 	if self._anim_state and not self._ext_camera:anim_state_machine():is_playing(self._anim_state) then
 		self._anim_state = nil
-		try_play_start_running(self)
-	end
+
+		if self._anim_state_callback then
+			local callback = self._anim_state_callback
+			self._anim_state_callback = nil
+			callback(self, t)
+		end
+	end	
 end)
